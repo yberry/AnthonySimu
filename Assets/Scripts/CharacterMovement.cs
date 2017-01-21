31977b126e25c573f3f687b94b87e3e42079d753
Mosphore <dorianessamir.w@gmail.com>
2017-01-21 04:18:23 +0100

    perso qui bouge animé

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 2;
+
+    private Vector2 movement;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        movement = Vector2.zero;
+
+        movement.x += Input.GetAxisRaw("Horizontal");
+
+        movement.y += Input.GetAxisRaw("Vertical");
+
+        Debug.Log(movement);
+
+        if (movement != Vector2.zero)
+        {
+            GetComponent<Animator>().SetBool("Walking", true);
+            GetComponent<Animator>().SetFloat("moveX", movement.x);
+            GetComponent<Animator>().SetFloat("moveY", movement.y);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Walking", false);
+        }
+        // follow this pattern
+
+        movement *= speed * Time.deltaTime;
+
+        transform.position = new Vector3(transform.position.x + movement.x, transform.position.y + movement.y, transform.position.z);
+    }
+}
