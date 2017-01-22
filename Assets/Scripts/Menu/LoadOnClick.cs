900baec6e8ffbe63548a7c2bc93d9a58c20631e7
yberry <y_berry@hotmail.fr>
2017-01-22 03:32:16 +0100

    Modifs menu

--- Assets/Scripts/Menu/LoadOnClick.cs
+++ Assets/Scripts/Menu/LoadOnClick.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
 
 	// Use this for initialization
 	public void LoadScene (int level) {
-        Application.LoadLevel(level);
+        SceneManager.LoadScene(level);
 	}
 }
