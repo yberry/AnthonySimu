1418d5924a80b4c6f17c78bb5316691d07441bb6
Maxime Aupetit <maxaupetit@hotmail.fr>
2017-01-22 03:39:42 +0100

    LD + Menu

---
+++ Assets/Scripts/Menu/ExitCreditOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitCreditOnClick : MonoBehaviour {
+
+
+    public GameObject credit;
+    public GameObject canvas;
+
+    void Start()
+    {
+        credit.SetActive(true);
+    }
+
+    public void StopCreditOnClick()
+    {
+        if (credit.activeSelf == true)
+        {
+            credit.SetActive(false);
+            canvas.SetActive(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Menu/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadScene (int level) {
+        Application.LoadLevel(level);
+	}
+}
